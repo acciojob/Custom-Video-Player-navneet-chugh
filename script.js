1fc8ca0f2c527609a2eee0eaa140a188efa03096
navneet-chugh <navneetchugh15@gmail.com>
2023-09-02 23:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,45 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+// Play/pause the video
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        toggleButton.textContent = '❚ ❚';
+    } else {
+        video.pause();
+        toggleButton.textContent = '►';
+    }
+}
+
+// Update the progress bar
+function updateProgressBar() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+// Skip ahead or backward
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Set volume and playback rate
+function handleSliderUpdate() {
+    video[this.name] = this.value;
+}
+
+// Add event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', () => toggleButton.textContent = '❚ ❚');
+video.addEventListener('pause', () => toggleButton.textContent = '►');
+video.addEventListener('timeupdate', updateProgressBar);
+
+toggleButton.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+volumeSlider.addEventListener('input', handleSliderUpdate);
+playbackRateSlider.addEventListener('input', handleSliderUpdate);
+
+// Initialize progress bar
+video.addEventListener('loadedmetadata', updateProgressBar);
